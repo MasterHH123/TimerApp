ff29e2929fe3248ad08facfb83f09da8ee543fe2
Horacio H <hernandezh4@hotmail.com>
2024-03-19 11:26:33 -0700

    Initial Commit

---
+++ timer.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+def getTime():
+    timeEpoch = time.time()
+    #epoch aint readable for me man
+    timeRN = time.ctime(timeEpoch)
+    return timeRN
+
+def parseTime(date):
+    #day, month, dayNum, hour, min, ssss
+    print(date)
+    parts = date.split(' ')
+    parsedTime = {
+            "hour": parts[3].split(':')[0],
+            "minute": parts[3].split(':')[1],
+            "seconds": parts[3].split(':')[2],
+            "year": parts[4]
+    }
+    return parsedTime
+        
+
+print(f"Current time is: ", getTime())
+print(f"Parsed time is: ", parseTime(getTime()))
